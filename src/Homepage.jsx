bec376c89c84305432d53a6c0e6d7d513cb2eae8
Adadam Szyller <adamszyller@gmail.com>
2023-06-01 20:50:17 +0200

    Dynamiczna strona
    dynamiczna zmiana kontentu strony bez odświeżania całości :3
    i lekka poprawa css i guess
    psycha siada ale ide w dobro strone
    a i wsm dostosowanie strony do przyszlego wczytywania danych z serwera

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,23 +1,36 @@
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
+
+let show = 1;
 
 export function Homepage() {
 
-    return (
+  const [tiles, setTiles] = useState(ShowTiles());
+  
+  useEffect(() => {
+    setTiles(ShowTiles());
+  });
+
+  function ShowPosts(){
+    show = 1;
+  }
+
+  function ShowProfiles(){
+    show = 2;
+  }
+
+  return (
   <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>
     <div id="main">
       <div id="top">
         <div id="search">
-
         </div>
         <div id="navigation">
-          <div id="button"></div>
-          <div id="button"></div>
+          <div id="button" onClick={ShowPosts}></div>
+          <div id="button" onClick={ShowProfiles}></div>
         </div>
         <div id="user">
-
         </div>
-
       </div>
       <div>
         <div id="left">
@@ -25,21 +38,44 @@ export function Homepage() {
           <div id="button"></div>
           <div id="button"></div>
         </div>
-        <div id="middle">
-          <div id="post"><div id="button"><div id="profile_pic"></div><a>user</a></div></div>
-          <div id="post"><div id="button"></div></div>
-          <div id="person"><div id="profile_pic"><div id="button"></div></div><div id="add"><div id="button"></div></div><div id="delete"><div id="button"></div></div></div>
-          <div id="person"></div>
-          <div id="person"></div>
-          <div id="person"></div>
-        </div>
+        <div id="middle">{tiles}</div>
         <div id="right">
-          <div id="button"></div>
-          <div id="button"></div>
-          <div id="button"></div>
+          <FriendButton />
         </div>
       </div>
     </div>
   </body>
-);
+    );
+}
+
+function TilePost() {
+  return(
+    <div id="post"><div id="button"><div id="profile_pic"></div><a>user</a></div></div>
+  )
+}
+
+function TileProfile(){
+  return(
+    <div id="person"><div id="profile_pic"><div id="button"></div></div><div id="add"><div id="button"></div></div><div id="delete"><div id="button"></div></div></div>
+  )
+}
+
+function ShowTiles(){
+  
+  if (show === 1){
+    return (
+      <><TilePost /></>
+    )
+  }
+  if (show === 2){
+    return (
+      <><TileProfile /></>
+    )
+  }
+}
+
+function FriendButton(){
+  return (
+    <div id="button"></div>
+  )
 }
